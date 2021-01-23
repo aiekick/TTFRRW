07ee19467ae7a5022624377c445e14e4dd328021
Aiekick <aiekick@funparadigm.com>
2021-01-23 21:24:55 +0100

    -

--- ttfrrw.cpp
+++ ttfrrw.cpp
@@ -1,11 +1,415 @@
 #include "ttfrrw.h"
 
-ttfrrw::ttfrrw()
+#include <fstream>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <cerrno>
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+ttfrrw::MemoryStream::MemoryStream()
 {
 
 }
 
-ttfrrw::~ttfrrw()
+ttfrrw::MemoryStream::MemoryStream(uint8_t * vDatas, size_t vSize)
+{
+	Set(vDatas, vSize);
+}
+
+ttfrrw::MemoryStream::~MemoryStream()
 {
 
+}
+
+void ttfrrw::MemoryStream::WriteByte(uint8_t b)
+{
+	m_Datas.push_back(b);
+}
+
+void ttfrrw::MemoryStream::WriteBytes(std::vector<uint8_t> *buffer)
+{
+	if (buffer)
+	{
+		m_Datas.insert(m_Datas.end(), buffer->begin(), buffer->end());
+	}
+}
+
+void ttfrrw::MemoryStream::WriteInt(int32_t i)
+{
+	WriteByte((uint8_t)((i >> 24) & 0xff));
+	WriteByte((uint8_t)((i >> 16) & 0xff));
+	WriteByte((uint8_t)((i >> 8) & 0xff));
+	WriteByte((uint8_t)(i & 0xff));
+}
+
+void ttfrrw::MemoryStream::WriteUShort(int32_t us)
+{
+	WriteByte((uint8_t)((us >> 8) & 0xff));
+	WriteByte((uint8_t)(us & 0xff));
+}
+
+void ttfrrw::MemoryStream::WriteFWord(int32_t us)
+{
+	WriteUShort(us);
+}
+
+void ttfrrw::MemoryStream::WriteShort(int32_t s)
+{
+	WriteUShort(s);
+}
+
+void ttfrrw::MemoryStream::WriteUInt24(int32_t ui)
+{
+	WriteByte((uint8_t)(ui >> 16) & 0xff);
+	WriteByte((uint8_t)(ui >> 8) & 0xff);
+	WriteByte((uint8_t)ui & 0xff);
+}
+
+void ttfrrw::MemoryStream::WriteULong(int64_t ul)
+{
+	WriteByte((uint8_t)((ul >> 24) & 0xff));
+	WriteByte((uint8_t)((ul >> 16) & 0xff));
+	WriteByte((uint8_t)((ul >> 8) & 0xff));
+	WriteByte((uint8_t)(ul & 0xff));
+}
+
+void ttfrrw::MemoryStream::WriteLong(int64_t l)
+{
+	WriteULong(l);
+}
+
+void ttfrrw::MemoryStream::WriteFixed(MemoryStream::Fixed f)
+{
+	WriteByte((uint8_t)((f.high >> 24) & 0xff));
+	WriteByte((uint8_t)((f.high >> 16) & 0xff));
+	WriteByte((uint8_t)((f.low >> 8) & 0xff));
+	WriteByte((uint8_t)(f.low & 0xff));
+}
+
+void ttfrrw::MemoryStream::WriteF2DOT14(MemoryStream::F2DOT14 f)
+{
+	WriteShort(f.value);
+}
+
+void ttfrrw::MemoryStream::WriteDateTime(longDateTime date)
+{
+	WriteULong((date >> 32) & 0xffffffff);
+	WriteULong(date & 0xffffffff);
+}
+
+uint8_t* ttfrrw::MemoryStream::Get()
+{
+	return m_Datas.data();
+}
+
+size_t ttfrrw::MemoryStream::Size()
+{
+	return m_Datas.size();
+}
+
+size_t ttfrrw::MemoryStream::GetPos()
+{
+	return m_ReadPos;
+}
+
+void ttfrrw::MemoryStream::SetPos(size_t vPos)
+{
+	m_ReadPos = vPos;
+}
+
+void ttfrrw::MemoryStream::Set(uint8_t * vDatas, size_t vSize)
+{
+	if (vDatas && vSize)
+	{
+		m_Datas.clear();
+		m_Datas.resize(vSize);
+
+		memcpy(m_Datas.data(), vDatas, vSize);
+	}
+}
+
+uint8_t ttfrrw::MemoryStream::ReadByte()
+{
+	if (m_ReadPos < m_Datas.size())
+		return m_Datas[m_ReadPos++];
+	return 0;
+}
+
+int32_t ttfrrw::MemoryStream::ReadUShort()
+{
+	return 0xffff & (ReadByte() << 8 | ReadByte());
+}
+
+int32_t ttfrrw::MemoryStream::ReadShort()
+{
+	return ((ReadByte() << 8 | ReadByte()) << 16) >> 16;
+}
+
+ttfrrw::MemoryStream::FWord ttfrrw::MemoryStream::ReadFWord()
+{
+	return (int16_t)ReadShort();
+}
+
+uint32_t ttfrrw::MemoryStream::ReadUInt24()
+{
+	return 0xffffff & (ReadByte() << 16 | ReadByte() << 8 | ReadByte());
+}
+
+uint64_t ttfrrw::MemoryStream::ReadULong()
+{
+	return 0xffffffffL & ReadLong();
+}
+
+uint32_t ttfrrw::MemoryStream::ReadULongAsInt()
+{
+	int64_t ulong = ReadULong();
+	return ((int32_t)ulong) & ~0x80000000;
+}
+
+int32_t ttfrrw::MemoryStream::ReadLong()
+{
+	return ReadByte() << 24 | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
+}
+
+ttfrrw::MemoryStream::Fixed ttfrrw::MemoryStream::ReadFixed()
+{
+	Fixed res;
+	int32_t f = ReadLong();
+	res.high = (int16_t)((f >> 16) & 0xff);
+	res.low = (int16_t)(f & 0xff);
+	return res;
+}
+
+ttfrrw::MemoryStream::F2DOT14 ttfrrw::MemoryStream::ReadF2DOT14()
+{
+	F2DOT14 res;
+	res.value = (int16_t)ReadShort();
+	return res;
+}
+
+ttfrrw::MemoryStream::longDateTime ttfrrw::MemoryStream::ReadDateTime()
+{
+	return (int64_t)ReadULong() << 32 | ReadULong();
+}
+
+std::string ttfrrw::MemoryStream::ReadString(size_t vLen)
+{
+	std::string res = std::string((char*)(m_Datas.data() + m_ReadPos), vLen);
+	m_ReadPos += vLen;
+	return res;
+}
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+ttfrrw::Curve::Curve()
+{
+
+}
+
+ttfrrw::Curve::~Curve()
+{
+
+}
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+ttfrrw::AffineGlyph::AffineGlyph()
+{
+
+}
+
+ttfrrw::AffineGlyph::~AffineGlyph()
+{
+
+}
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+ttfrrw::Glyph::Glyph()
+{
+
+}
+
+ttfrrw::Glyph::~Glyph()
+{
+
+}
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+ttfrrw::ttfrrw::ttfrrw()
+{
+
+}
+
+ttfrrw::ttfrrw::~ttfrrw()
+{
+
+}
+
+bool ttfrrw::ttfrrw::OpenFontFile(const std::string& vFontFilePathName)
+{
+	(void)vFontFilePathName;
+	
+	return false;
+}
+
+bool ttfrrw::ttfrrw::WriteFontFile(const std::string& vFontFilePathName)
+{
+	return "";
+}
+
+///////////////////////////////////////////////////////////////////////
+//// PUBLIC METHOD'S //////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+void ttfrrw::ttfrrw::AddGlyph(const Glyph& vGlyph, const CodePoint& vCodePoint)
+{
+	(void)vGlyph;
+	(void)vCodePoint;
+}
+
+ttfrrw::Glyph ttfrrw::ttfrrw::GetGlyph(const CodePoint& vCodePoint)
+{
+	(void)vCodePoint;
+
+	Glyph res;
+	return res;
+}
+
+///////////////////////////////////////////////////////////////////////
+//// PRIVATE FILE / STREAM ////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+bool ttfrrw::ttfrrw::LoadFileToMemory(
+	const std::string& vFilePathName, 
+	MemoryStream* vInMem,
+	int* vError)
+{
+	bool res = false;
+
+	if (vInMem)
+	{
+		std::vector<uint8_t> bytes;
+
+		FILE* intput_file = NULL;
+#if defined(MSVC)
+		fopen_s(&intput_file, vFilePathName.c_str(), "rb");
+#else
+		intput_file = fopen(vFilePathName.c_str(), "rb");
+#endif
+		if (vError)
+			*vError = errno;
+		if (intput_file != reinterpret_cast<FILE*>(NULL))
+		{
+			long fileSize = 0;
+			// obtain file size:
+			fseek(intput_file, 0, SEEK_END);
+			fileSize = ftell(intput_file);
+			rewind(intput_file);
+
+			// copy the file into the buffer and close
+			bytes.resize(fileSize);
+			fread(bytes.data(), 1, fileSize, intput_file);
+			vInMem->Set(bytes.data(), bytes.size());
+			fclose(intput_file);
+
+			res = true;
+		}
+	}
+
+	return res;
+}
+
+bool ttfrrw::ttfrrw::WriteMemoryToFile(
+	const std::string& vFilePathName, 
+	MemoryStream* vOutMem,
+	int* vError)
+{
+	bool res = false;
+
+	if (vOutMem)
+	{
+		if (vOutMem->Get() && vOutMem->Size())
+		{
+			FILE* output_file = nullptr;
+#if defined(MSVC)
+			fopen_s(&output_file, vFilePathName.c_str(), "wb");
+#else
+			output_file = fopen(vFilePathName.c_str(), "wb");
+#endif
+			if (vError)
+				*vError = errno;
+			if (output_file != reinterpret_cast<FILE*>(NULL))
+			{
+				fwrite(vOutMem->Get(), 1, vOutMem->Size(), output_file);
+				fflush(output_file);
+				fclose(output_file);
+				res = true;
+			}
+		}
+	}
+	
+	return res;
+}
+
+///////////////////////////////////////////////////////////////////////
+//// PRIVATE TABLES ///////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+bool ttfrrw::ttfrrw::Assemble_GLYF_Table()
+{
+	return false;
+}
+
+bool ttfrrw::ttfrrw::Assemble_LOCA_Table()
+{
+	return false;
+}
+
+bool ttfrrw::ttfrrw::Assemble_MAXP_Table()
+{
+	return false;
+}
+
+bool ttfrrw::ttfrrw::Assemble_CMAP_Table()
+{
+	return false;
+}
+
+bool ttfrrw::ttfrrw::Assemble_HMTX_Table()
+{
+	return false;
+}
+
+bool ttfrrw::ttfrrw::Assemble_HHEA_Table()
+{
+	return false;
+}
+
+bool ttfrrw::ttfrrw::Assemble_POST_Table()
+{
+	return false;
+}
+
+bool ttfrrw::ttfrrw::Assemble_NAME_Table()
+{
+	return false;
+}
+
+bool ttfrrw::ttfrrw::Assemble_HEAD_Table()
+{
+	return false;
 }
--- ttfrrw.h
+++ ttfrrw.h
@@ -1,8 +1,354 @@
 #pragma once
 
-class ttfrrw
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility> // pair
+#include <cmath>
+
+namespace ttfrrw
 {
-public:
-	ttfrrw();
-	~ttfrrw();
-};
+	///////////////////////////////////////////////////////////////////////
+	///// COMMON///////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////
+
+	template <typename T> inline T round(const T& v) { return (T)(::std::round((double)v)); }
+	template <typename T> inline T floor(const T& v) { return (T)(::std::floor((double)v)); }
+	template <typename T> inline T ceil(const T& v) { return (T)(::std::ceil((double)v)); }
+	template <typename T> inline T fract(const T& v) { return v - floor<T>(v); }
+	template <typename T> inline T cos(const T& v) { return ::std::cos(v); }
+	template <typename T> inline T sin(const T& v) { return ::std::sin(v); }
+	template <typename T> inline T tan(const T& v) { return ::std::tan(v); }
+	template <typename T> inline T atan(const T& v) { return ::std::atan(v); }
+	template <typename T> inline T mini(const T& a, const T& b) { return a < b ? a : b; }
+	template <typename T> inline T maxi(const T& a, const T& b) { return a > b ? a : b; }
+	template <typename T> inline T clamp(const T& n) { return n >= T(0) && n <= T(1) ? n : T(n > T(0)); } // clamp n => 0 to 1
+	template <typename T> inline T clamp(const T& n, const T& b) { return n >= T(0) && n <= b ? n : T(n > T(0)) * b; } // clamp n => 0 to b
+	template <typename T> inline T clamp(const T& n, const T& a, const T& b) { return n >= a && n <= b ? n : n < a ? a : b; } // clamp n => a to b
+	template <typename T> inline T abs(const T& a) { return a < 0 ? a * (T)-1 : a; }
+	template <typename T> inline T sign(const T& a) { return a < 0 ? (T)-1 : (T)1; }
+	template <typename T> inline T step(const T& a, const T& b) { return b < a ? (T)0 : (T)1; }
+	template <typename T> inline T mod(const T& v, const T& l) { return ::std::modf(v, l); }
+	template <typename T> inline T invMix(const T& i, const T& s, const T& r) { return (r - i) / (s - i); }
+	template <typename T> inline T lerp(const T& a, const T& b, const T& t) { return a * (1.0f - t) + b * t; }
+	template <typename T> inline T eerp(const T& a, const T& b, const T& t) { if (a == (T)0) return T(0); return pow(a * (b / a), t); }
+	template <typename T> inline T mix(const T& a, const T& b, const T& t) { return lerp(a, b, t); }
+
+	///////////////////////////////////////////////////////////////////////
+	///// VEC2 ////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////
+
+	template <typename T>
+	struct vec2
+	{
+		T x, y;
+		vec2<T>() { x = (T)0; y = (T)0; }
+		template <typename U> vec2<T>(vec2<U> a) { x = (T)a.x; y = (T)a.y; }
+		vec2<T>(T a) { x = a; y = a; }
+		vec2<T>(T a, T b) { x = a; y = b; }
+		T operator () (size_t i) const { return (&x)[i]; }
+		T operator [] (size_t i) const { return (&x)[i]; }
+		T& operator () (size_t i) { return (&x)[i]; }
+		void operator ++ () { x++; y++; }
+		void operator -- () { x--; y--; }
+		void operator += (const vec2<T>& v) { x += v.x; y += v.y; }
+		void operator -= (const vec2<T>& v) { x -= v.x; y -= v.y; }
+		bool operator == (const vec2<T>& v) { return (x == v.x) && (y == v.y); }
+		bool operator != (const vec2<T>& v) { return (x != v.x) || (y != v.y); }
+		void operator *= (T a) { x *= a; y *= a; }
+		void operator *= (vec2<T> v) { x *= v.x; y *= v.y; }
+		void operator /= (T a) { x /= a; y /= a; }
+		void operator /= (vec2<T> v) { x /= v.x; y /= v.y; }
+		T length() { return sqrt(lengthSquared()); }
+		T lengthSquared() { return x * x + y * y; }
+		T normalize() { T _length = length(); if (_length < (T)1e-5) return (T)0.0; T _invLength = (T)1.0 / _length; x *= _invLength; y *= _invLength; return _length; } // return length
+		vec2<T> GetNormalized() { vec2<T> n = vec2<T>(x, y); n.normalize(); return n; }
+		T sum() { return x + y; }
+		T sumAbs() { return abs<T>(x) + abs<T>(y); }
+		bool emptyAND() { return x == (T)0 && y == (T)0; }
+		bool emptyOR() { return x == (T)0 || y == (T)0; }
+		std::string string(char c = ';') { return toStr(x) + c + toStr(y); }
+		T ratioXY() { if (y > (T)0) return x / y; return (T)0; }
+		T ratioYX() { if (x > (T)0) return y / x; return (T)0; }
+	};
+	template <typename T> inline vec2<T> operator + (vec2<T> v, T f) { return vec2<T>(v.x + f, v.y + f); }
+	template <typename T> inline vec2<T> operator + (T f, vec2<T> v) { return vec2<T>(v.x + f, v.y + f); }
+	template <typename T> inline vec2<T> operator + (vec2<T> v, vec2<T> f) { return vec2<T>(v.x + f.x, v.y + f.y); }
+	template <typename T> inline vec2<T> operator - (vec2<T> v, T f) { return vec2<T>(v.x - f, v.y - f); }
+	template <typename T> inline vec2<T> operator - (T f, vec2<T> v) { return vec2<T>(f - v.x, f - v.y); }
+	template <typename T> inline vec2<T> operator - (vec2<T> v, vec2<T> f) { return vec2<T>(v.x - f.x, v.y - f.y); }
+	template <typename T> inline vec2<T> operator * (vec2<T> v, T f) { return vec2<T>(v.x * f, v.y * f); }
+	template <typename T> inline vec2<T> operator * (T f, vec2<T> v) { return vec2<T>(v.x * f, v.y * f); }
+	template <typename T> inline vec2<T> operator * (vec2<T> v, vec2<T> f) { return vec2<T>(v.x * f.x, v.y * f.y); }
+	template <typename T> inline vec2<T> operator / (vec2<T> v, T f) { return vec2<T>(v.x / f, v.y / f); }
+	template <typename T> inline vec2<T> operator / (T f, vec2<T> v) { return vec2<T>(f / v.x, f / v.y); }
+	template <typename T> inline vec2<T> operator / (vec2<T> v, vec2<T> f) { return vec2<T>(v.x / f.x, v.y / f.y); }
+	template <typename T> inline bool operator < (vec2<T> v, vec2<T> f) { return v.x < f.x&& v.y < f.y; }
+	template <typename T> inline bool operator < (vec2<T> v, T f) { return v.x < f&& v.y < f; }
+	template <typename T> inline bool operator > (vec2<T> v, vec2<T> f) { return v.x > f.x && v.y > f.y; }
+	template <typename T> inline bool operator > (vec2<T> v, T f) { return v.x > f && v.y > f; }
+	template <typename T> inline bool operator <= (vec2<T> v, vec2<T> f) { return v.x <= f.x && v.y <= f.y; }
+	template <typename T> inline bool operator <= (vec2<T> v, T f) { return v.x <= f && v.y <= f; }
+	template <typename T> inline bool operator >= (vec2<T> v, vec2<T> f) { return v.x >= f.x && v.y >= f.y; }
+	template <typename T> inline bool operator >= (vec2<T> v, T f) { return v.x >= f && v.y >= f; }
+	template <typename T> inline bool operator != (vec2<T> v, vec2<T> f) { return f.x != v.x || f.y != v.y; }
+	template <typename T> inline bool operator == (vec2<T> v, vec2<T> f) { return f.x == v.x && f.y == v.y; }
+	template <typename T> inline vec2<T> floor(vec2<T> a) { return vec2<T>(floor(a.x), floor(a.y)); }
+	template <typename T> inline vec2<T> fract(vec2<T> a) { return vec2<T>(fract(a.x), fract(a.y)); }
+	template <typename T> inline vec2<T> ceil(vec2<T> a) { return vec2<T>(ceil(a.x), ceil(a.y)); }
+	template <typename T> inline vec2<T> mini(vec2<T> a, vec2<T> b) { return vec2<T>(mini(a.x, b.x), mini(a.y, b.y)); }
+	template <typename T> inline vec2<T> maxi(vec2<T> a, vec2<T> b) { return vec2<T>(maxi(a.x, b.x), maxi(a.y, b.y)); }
+	template <typename T> inline T dot(vec2<T> a, vec2<T> b) { return a.x * b.x + a.y * b.y; }
+	template <typename T> inline T det(vec2<T> a, vec2<T> b) { return a.x * b.y - a.y * b.x; }
+	template <typename T> inline vec2<T> cross(vec2<T> a, vec2<T> b) { return vec2<T>(a.x * b.y - a.y * b.x, a.y * b.x - a.x * b.y); }
+	template <typename T> inline vec2<T> reflect(vec2<T> I, vec2<T> N) { return I - (T)2 * dotS(N, I) * N; }
+	template <typename T> inline vec2<T> sign(vec2<T> a) { return vec2<T>(a.x < (T)0 ? -(T)1 : (T)1, a.y < (T)0 ? (T)-1 : (T)1); }
+	template <typename T> inline vec2<T> sin(vec2<T> a) { return vec2<T>(sin<T>(a.x), sin<T>(a.y)); }
+	template <typename T> inline vec2<T> cos(vec2<T> a) { return vec2<T>(cos<T>(a.x), cos<T>(a.y)); }
+	template <typename T> inline vec2<T> tan(vec2<T> a) { return vec2<T>(tan<T>(a.x), tan<T>(a.y)); }
+	template <typename T> inline vec2<T> atan(vec2<T> a) { return vec2<T>(atan<T>(a.x), atan<T>(a.y)); }
+	typedef vec2<double> dvec2;
+	typedef vec2<float> fvec2;
+	typedef vec2<bool> bvec2;
+	typedef vec2<int8_t> i8vec2;
+	typedef vec2<int16_t> i16vec2;
+	typedef vec2<int32_t> ivec2;
+	typedef vec2<int32_t> i32vec2;
+	typedef vec2<int64_t> i64vec2;
+	typedef vec2<uint8_t> u8vec2;
+	typedef vec2<uint16_t> u16vec2;
+	typedef vec2<uint32_t> uvec2;
+	typedef vec2<uint32_t> u32vec2;
+	typedef vec2<uint64_t> u64vec2;
+	
+	///////////////////////////////////////////////////////////////////////
+	///// AABB ////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////
+
+	template <typename T>
+	struct AABB // copy of b2AABB struct
+	{
+		vec2<T> lowerBound;	///< the lower left vertex
+		vec2<T> upperBound;	///< the upper right vertex
+		AABB() : lowerBound((T)0), upperBound((T)0) {}
+		AABB(vec2<T> vlowerBound, vec2<T> vUpperBound) : lowerBound(vlowerBound), upperBound(vUpperBound) {}
+		//may be in format "0.2f,0.3f,0.4f,0.8f" left, bottom, right, top
+		AABB(::std::string vec, char c) { ::std::vector<float> result = StringToNumberVector<float>(vec, c); size_t s = result.size(); if (s > 0) lowerBound.x = result[0]; if (s > 1) lowerBound.y = result[1]; if (s > 2) upperBound.x = result[2]; if (s > 3) upperBound.y = result[3]; }
+		/// Add a vector to this vector.
+		void operator += (const vec2<T>& v) { lowerBound += v; upperBound += v; }
+		/// Subtract a vector from this vector.
+		void operator -= (const vec2<T>& v) { lowerBound -= v; upperBound -= v; }
+		/// Multiply this vector by a scalar.
+		void operator *= (T a) { lowerBound *= a; upperBound *= a; }
+		/// Divide this vector by a scalar.
+		void operator /= (T a) { lowerBound /= a; upperBound /= a; }
+		/// Get the center of the AABB.
+		vec2<T> GetCenter() const { return (lowerBound + upperBound) / (T)2; }
+		/// Get the extents of the AABB (half-widths).
+		vec2<T> GetExtents() const { return (upperBound - lowerBound) / (T)2; }
+		/// Get the perimeter length
+		T GetPerimeter() const { float wx = upperBound.x - lowerBound.x; float wy = upperBound.y - lowerBound.y; return (T)2 * (wx + wy); }
+		/// Combine an AABB into this one.
+		void Combine(const AABB<T>& aabb) { lowerBound = mini(lowerBound, aabb.lowerBound); upperBound = maxi(upperBound, aabb.upperBound); }
+		/// Combine two AABBs into this one.
+		void Combine(const AABB<T>& aabb1, const AABB<T>& aabb2) { lowerBound = mini(aabb1.lowerBound, aabb2.lowerBound); upperBound = maxi(aabb1.upperBound, aabb2.upperBound); }
+		/// Combine a point into this one.
+		void Combine(const vec2<T>& pt) { lowerBound.x = mini(lowerBound.x, pt.x); lowerBound.y = mini(lowerBound.y, pt.y); upperBound.x = maxi(upperBound.x, pt.x); upperBound.y = maxi(upperBound.y, pt.y); }
+		/// Set vlowerBound at mini and vUpperBound at maxi
+		void Set(vec2<T> vlowerBound, vec2<T> vUpperBound) { lowerBound.x = mini(vlowerBound.x, vUpperBound.x); lowerBound.y = mini(vlowerBound.y, vUpperBound.y); upperBound.x = maxi(vlowerBound.x, vUpperBound.x); upperBound.y = maxi(vlowerBound.y, vUpperBound.y); }
+		/// Does this aabb contain the provided AABB.
+		bool Contains(const AABB<T>& aabb) const { bool result = true; result = result && lowerBound.x <= aabb.lowerBound.x; result = result && lowerBound.y <= aabb.lowerBound.y; result = result && aabb.upperBound.x <= upperBound.x; result = result && aabb.upperBound.y <= upperBound.y; return result; }
+		/// Does this aabb contain the provided vec2<T>.
+		bool ContainsPoint(const vec2<T>& pt) const { bool result = true; result = result && lowerBound.x <= pt.x; result = result && lowerBound.y <= pt.y; result = result && pt.x <= upperBound.x; result = result && pt.y <= upperBound.y; return result; }
+		bool Intersects(const AABB<T>& other) { bool result = true; result = result || lowerBound.x <= other.lowerBound.x; result = result || lowerBound.y <= other.lowerBound.y; result = result || other.upperBound.x <= upperBound.x; result = result || other.upperBound.y <= upperBound.y; return result; }
+		vec2<T> Size() { return vec2<T>(upperBound - lowerBound); }
+	};
+	typedef AABB<int> iAABB;
+	typedef AABB<double> dAABB;
+	typedef AABB<float> fAABB;
+
+	///////////////////////////////////////////////////////////////////////
+	///// MEMORY STREAM ///////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////
+
+	class MemoryStream
+	{
+	public:
+		struct Fixed // its like a float with int32 : high.low ex : 1 high 0 low => 1.0
+		{
+			int16_t high = 0;
+			int16_t low = 0;
+		};
+		struct F2DOT14
+		{
+			int16_t value = 0;
+			F2DOT14() { value = 0; }
+			F2DOT14(int16_t v) { value = v; }
+			F2DOT14 operator = (int16_t v) { value = v; }
+			void SetFloat(float vValue) { value = (int16_t)roundf(vValue * 16384.f); }
+			float GetFloat() { return (float)(value >> 14) + (float)(value & 0x3FFF) / 16384.0f; }
+		};
+		typedef uint16_t UFWord;
+		typedef int16_t FWord;
+		typedef int64_t longDateTime;
+
+	public:
+		MemoryStream();
+		MemoryStream(uint8_t* vDatas, size_t vSize);
+		~MemoryStream();
+
+		void WriteByte(uint8_t b);
+		void WriteBytes(std::vector<uint8_t>* buffer);
+		void WriteShort(int32_t i);
+		void WriteUShort(int32_t us);
+		void WriteFWord(int32_t us);
+		void WriteInt(int32_t i);
+		void WriteUInt24(int32_t ui);
+		void WriteULong(int64_t ul);
+		void WriteLong(int64_t l);
+		void WriteFixed(Fixed f);
+		void WriteF2DOT14(F2DOT14 f);
+		void WriteDateTime(longDateTime date);
+
+		uint8_t* Get();
+		void Set(uint8_t* vDatas, size_t vSize);
+
+		size_t Size();
+
+		size_t GetPos();
+		void SetPos(size_t vPos);
+
+		uint8_t ReadByte();
+		int32_t ReadUShort();
+		int32_t ReadShort();
+		FWord ReadFWord();
+		uint32_t ReadUInt24();
+		uint64_t ReadULong();
+		uint32_t ReadULongAsInt();
+		int32_t ReadLong();
+		Fixed ReadFixed();
+		F2DOT14 ReadF2DOT14();
+		longDateTime ReadDateTime();
+		std::string ReadString(size_t vLen);
+
+	private:
+		std::vector<uint8_t> m_Datas;
+		size_t m_ReadPos = 0;
+	};
+
+	///////////////////////////////////////////////////////////////////////
+	///// CURVE ///////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////
+
+	class Curve
+	{
+	public:
+		std::vector<fvec2> m_Points;
+
+	public:
+		Curve();
+		~Curve();
+	};
+
+	///////////////////////////////////////////////////////////////////////
+	///// GLYPH ///////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////
+
+	class BaseGlyph
+	{
+	public:
+		std::vector<Curve> m_Curves;
+		iAABB m_LocalBBox;
+		int32_t m_AdvanceX = 0;
+		int32_t m_LeftSideBearing = 0;
+	};
+
+	class AffineGlyph : public BaseGlyph
+	{
+	public:
+		fvec2 m_Translation;
+		fvec2 m_Scale;
+
+	public:
+		AffineGlyph();
+		~AffineGlyph();
+	};
+
+	class Glyph : public BaseGlyph
+	{
+	public:
+		std::vector<AffineGlyph> m_AffineGlyphs; // composite or color layers
+		
+	public:
+		Glyph();
+		~Glyph();
+	};
+
+	///////////////////////////////////////////////////////////////////////
+	///// MAIN CLASS TTFRRW ///////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////
+
+	typedef uint32_t CodePoint;
+	typedef uint32_t GlyphIndex;
+	
+	class ttfrrw
+	{
+	private: // will be computed by ttfrrw
+		iAABB m_GlobalBBox;
+		int32_t m_AdvanceWidthMax = 0;
+		int32_t m_MinLeftSideBearing = 0;
+		int32_t m_MinRightSideBearing = 0;
+
+	private: // must be defined by user
+		std::vector<Glyph> m_Glyphs; // bd des glyph
+		std::map<CodePoint, GlyphIndex> m_CodePoints;
+		
+	public: // must be defined by user
+		int32_t m_Ascent = 0;
+		int32_t m_Descent = 0;
+		int32_t m_LineGap = 0;
+		
+		std::string m_FontCopyright;
+		std::string m_FontFamily;
+		std::string m_FontSubFamily;
+		std::string m_FontFullName;
+		std::string m_FontTrademark;
+		std::string m_FontDesignerName;
+		std::string m_FontManufacturerName;
+		std::string m_UrlFontDesigner;
+		std::string m_UrlFontVendor;
+
+	public:
+		ttfrrw();
+		~ttfrrw();
+
+	public:
+		bool OpenFontFile(const std::string& vFontFilePathName);
+
+	public:
+		bool WriteFontFile(const std::string& vFontFilePathName);
+
+	public:
+		void AddGlyph(const Glyph& vGlyph, const CodePoint& vCodePoint);
+		Glyph GetGlyph(const CodePoint& vCodePoint);
+
+	private:
+		bool LoadFileToMemory(const std::string& vFilePathName, MemoryStream* vInMem, int* vError);
+		bool WriteMemoryToFile(const std::string& vFilePathName, MemoryStream* vOutMem, int* vError);
+
+	private: // write table
+		bool Assemble_GLYF_Table();
+		bool Assemble_LOCA_Table();
+		bool Assemble_MAXP_Table();
+		bool Assemble_CMAP_Table();
+		bool Assemble_HMTX_Table();
+		bool Assemble_HHEA_Table();
+		bool Assemble_POST_Table();
+		bool Assemble_NAME_Table();
+		bool Assemble_HEAD_Table();
+	};
+}
